cd9fca5a6b02f599d26ab7415b6911bed91300c0
Jörg Jooss <joergjo@microsoft.com>
2023-12-19 15:52:09 +0100

    Remove hardcoded settings

--- go/clientv3/main.go
+++ go/clientv3/main.go
@@ -11,10 +11,7 @@ import (
 )
 
 const (
-	clientID = "client1-session1"
-	fqdn     = "mqtt-demo.westeurope-1.ts.eventgrid.azure.net"
-	topic    = "contosotopics/topic1"
-	v311     = 4
+	v311 = 4
 )
 
 func handler(client mqtt.Client, msg mqtt.Message) {
@@ -22,8 +19,8 @@ func handler(client mqtt.Client, msg mqtt.Message) {
 	log.Printf("Message: %s", msg.Payload())
 }
 
-func newTLSConfig() (*tls.Config, error) {
-	cert, err := tls.LoadX509KeyPair("./example.org.crt", "./example.org.key")
+func newTLSConfig(certFile, keyFile string) (*tls.Config, error) {
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
 		return nil, err
 	}
@@ -33,14 +30,14 @@ func newTLSConfig() (*tls.Config, error) {
 	}, nil
 }
 
-func newClientOptions(tlsConfig *tls.Config) *mqtt.ClientOptions {
+func newClientOptions(fqdn string, username string, clientID string, tlsConfig *tls.Config) *mqtt.ClientOptions {
 	uri := fmt.Sprintf("ssl://%s:8883", fqdn)
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(uri)
 	opts.SetClientID(clientID)
 	opts.SetDefaultPublishHandler(handler)
-	opts.SetUsername("client1-authnID")
-	opts.SetPassword("")
+	opts.SetUsername(username)
+	opts.SetPassword("") // we use a client certificate instead
 	opts.SetTLSConfig(tlsConfig)
 	opts.SetProtocolVersion(v311)
 	opts.OnConnect = func(c mqtt.Client) {
@@ -50,14 +47,39 @@ func newClientOptions(tlsConfig *tls.Config) *mqtt.ClientOptions {
 }
 
 func main() {
+	fqdn, ok := os.LookupEnv("MQTT_BROKER_FQDN")
+	if !ok {
+		log.Fatal("MQTT_BROKER_FQDN environment variable is not set")
+	}
+	topic, ok := os.LookupEnv("MQTT_TOPIC")
+	if !ok {
+		log.Fatal("MQTT_TOPIC environment variable is not set")
+	}
+	username, ok := os.LookupEnv("MQTT_USERNAME")
+	if !ok {
+		log.Fatal("MQTT_USERNAME environment variable is not set")
+	}
+	clientID, ok := os.LookupEnv("MQTT_CLIENT_ID")
+	if !ok {
+		log.Fatal("MQTT_CLIENT_ID environment variable is not set")
+	}
+	tlsCertFile, ok := os.LookupEnv("MQTT_TLS_CERT_FILE")
+	if !ok {
+		log.Fatal("MQTT_TLS_CERT_FILE environment variable is not set")
+	}
+	tlsKeyFile, ok := os.LookupEnv("MQTT_TLS_KEY_FILE")
+	if !ok {
+		log.Fatal("MQTT_TLS_KEY_FILE environment variable is not set")
+	}
+
 	mqtt.ERROR = log.New(os.Stdout, "", 0)
 	mqtt.CRITICAL = log.New(os.Stdout, "", 0)
 
-	tlsConfig, err := newTLSConfig()
+	tlsConfig, err := newTLSConfig(tlsCertFile, tlsKeyFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	opts := newClientOptions(tlsConfig)
+	opts := newClientOptions(fqdn, username, clientID, tlsConfig)
 	c := mqtt.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		log.Fatal(token.Error())
@@ -74,7 +96,7 @@ func main() {
 		time.Sleep(2 * time.Second)
 	}
 
-	time.Sleep(6 * time.Second)
+	time.Sleep(2 * time.Second)
 
 	if token := c.Unsubscribe(topic); token.Wait() && token.Error() != nil {
 		log.Fatal(token.Error())
