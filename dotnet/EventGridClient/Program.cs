4b0f96e87b78099df294bdf6e53e5cf873a8497d
Jörg Jooss <joergjo@microsoft.com>
2025-01-28 16:18:24 +0100

    Align C# implementation with Java version

--- dotnet/EventGridClient/Program.cs
+++ dotnet/EventGridClient/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using MQTTnet;
 using MQTTnet.Client;
+using MQTTnet.Exceptions;
+using MQTTnet.Protocol;
 
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -11,7 +13,7 @@ var builder = new ConfigurationBuilder()
     .AddCommandLine(args);
 var configuration = builder.Build();
 
-string[] keys = ["MQTT:BrokerFqdn", "MQTT:Username", "MQTT:ClientId", "MQTT:TlsCertFile", "MQTT:TlsKeyFile"];
+string[] keys = ["MQTT:BrokerFqdn", "MQTT:Topic", "MQTT:Username", "MQTT:ClientId", "MQTT:TlsCertFile", "MQTT:TlsKeyFile", "MQTT:CleanSession"];
 var isValidConfig = true;
 
 foreach (var key in keys)
@@ -30,41 +32,72 @@ if (!isValidConfig)
     return;
 }
 
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
+
 var certificate =
     new X509Certificate2(X509Certificate2.CreateFromPemFile(
         configuration["Mqtt:TlsCertFile"]!,
         configuration["Mqtt:TlsKeyFile"])
         .Export(X509ContentType.Pkcs12));
 
-var mqttClient = new MqttFactory().CreateMqttClient();
+using var mqttClient = new MqttFactory().CreateMqttClient();
 
 var connAck = await mqttClient!.ConnectAsync(new MqttClientOptionsBuilder()
     .WithTcpServer(configuration["Mqtt:BrokerFqdn"], 8883)
     .WithClientId(configuration["Mqtt:ClientId"])
     .WithCredentials(configuration["Mqtt:Username"], string.Empty)  //use client authentication name in the username
-    .WithTlsOptions(options => 
+    .WithTlsOptions(options =>
     {
         options.UseTls(true);
         options.WithClientCertificates([certificate]);
     })
+    .WithCleanSession(configuration["Mqtt:CleanSession"] == "true")
     .Build());
 
 Console.WriteLine($"Client connected: {mqttClient.IsConnected} with CONNACK: {connAck.ResultCode}");
 
-mqttClient.ApplicationMessageReceivedAsync +=
-    async m => await Console.Out.WriteAsync($"Received message on topic: '{m.ApplicationMessage.Topic}' with content: '{m.ApplicationMessage.ConvertPayloadToString()}'\n\n");
+mqttClient.DisconnectedAsync +=
+    async e => await Console.Out.WriteLineAsync($"Disconnected from the broker with reason: {e.Reason}");
+
+string topic = configuration["MQTT:Topic"]!;
 
 if (configuration.GetValue("Subscribe", false))
 {
-    await mqttClient.SubscribeAsync("contosotopics/topic1", MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce);
-    var suback = await mqttClient.SubscribeAsync("contosotopics/topic1");
+    mqttClient.ApplicationMessageReceivedAsync +=
+        async m => await Console.Out.WriteLineAsync($"Received message on topic: '{m.ApplicationMessage.Topic}' with content: '{m.ApplicationMessage.ConvertPayloadToString()}'\n\n");
+    Console.WriteLine("Subscribing to topic '{0}'", topic);
+    var suback = await mqttClient.SubscribeAsync(topic, MqttQualityOfServiceLevel.AtLeastOnce, cancellationToken: cts.Token);
     suback.Items.ToList().ForEach(s => Console.WriteLine($"subscribed to '{s.TopicFilter.Topic}' with '{s.ResultCode}'"));
 }
 
-Console.WriteLine("Sending messages");
-while (true)
+if (configuration.GetValue("Publish", false))
 {
-    var puback = await mqttClient.PublishStringAsync("contosotopics/topic1", $"Hello world at {DateTimeOffset.UtcNow:o}!");
-    Console.WriteLine(puback.ReasonString);
-    await Task.Delay(5000);
+    var message = configuration.GetValue("Message", "Hello MQTT from .NET");
+    Console.WriteLine("Publishing messages to topic'{0}'", topic);
+    try
+    {
+        for (int i = 1; !cts.IsCancellationRequested; i++)
+        {
+            var payload = $"{message} #{i} !";
+            var puback = await mqttClient.PublishStringAsync(topic, payload);
+            if (!puback.IsSuccess)
+            {
+                Console.WriteLine($"Failed to publish message: {payload}");
+            }
+            await Task.Delay(2000);
+        }
+    }
+    catch (MqttClientNotConnectedException)
+    {
+        Console.WriteLine("Client is not longer connected");
+    }
 }
+
+// Prevent the application from exiting immediately
+cts.Token.WaitHandle.WaitOne();
+Console.WriteLine("Application is terminating...");
